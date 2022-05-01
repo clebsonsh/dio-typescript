0d718038b354ef9a87c3977aff21fe819c2f1a89
Clebson Moura <clebson.sh@gmail.com>
2022-05-01 17:39:56 -0300

    Trabalhando com input tag

--- index.ts
+++ index.ts
@@ -1,37 +1,6 @@
-// ### Interfaces ###
+const input = document.getElementById('input') as HTMLInputElement
 
-// Cria uma interface
-interface IAnimal {
-  nome: string,
-  tipo: 'terrestre' | 'aquático',
-}
-
-// Declara um objeto usando a interface
-const Elefante: IAnimal = {
-  nome: 'Elefante',
-  tipo: 'terrestre',
-}
-
-// Extende um interface
-interface IFelino extends IAnimal {
-  visaoNoturna: boolean,
-} 
-
-interface ICanino extends IAnimal {
-  porte: 'pequeno' | 'medio' | 'grande'
-}
-
-const leao: IFelino = {
-  nome: 'Leão',
-  tipo: 'terrestre',
-  visaoNoturna: false,
-}
-
-// ### Types ###
-type IPet = IFelino | ICanino
-
-const dog: IPet = {
-  nome: 'doggy',
-  tipo: 'terrestre',
-  porte: 'pequeno',
-}
+input.addEventListener('input', (event) => {
+  const e = event.currentTarget as HTMLInputElement
+  console.log(e.value)
+})
